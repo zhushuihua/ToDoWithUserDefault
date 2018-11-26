4b35d055c73c2ded85f6199b6646841629103912
Joseph <null>
2018-11-27 00:58:41 +0800

    Complete Dummy Data Display

--- ToDoWithUserDefault/ViewController.swift
+++ ToDoWithUserDefault/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:UIViewController, UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet weak var tableView: UITableView!
+    var items:[String] = ["UMA", "The Best", "Dual-Hand Abacus"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
 }
 
